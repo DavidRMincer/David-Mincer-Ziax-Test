373d9ebd410b6cc8c1f3b36f16e81e9182666e8c
DavidRMincer <david.r.mincer@live.com>
2023-01-08 15:30:44 +0000

    New C++ Project

---
+++ David Mincer Ziax Test/David Mincer Ziax Test/David Mincer Ziax Test.cpp
@@ -0,0 +1,14 @@
+// David Mincer Ziax Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+	return 0;
+}
